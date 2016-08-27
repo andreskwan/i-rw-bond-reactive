9033100bb599f363e2abef460190bc703e839769
Andres Kwan <andreskwan@gmail.com>
2016-08-27 00:08:15 -0500

    [VM] - [add Comments] - [restore validation for text longitud]

--- BindingWithBond/PhotoSearchViewModel.swift
+++ BindingWithBond/PhotoSearchViewModel.swift
@@ -16,10 +16,18 @@ class PhotoSearchViewModel {
     init() {
         searchString.value = "Bond"
         
+        /*
+         Test - bidirectionalBindTo
+         */
         searchString.observeNew {
             text in
             print(text)
         }
+        /*
+         Validation: text longitud
+         */
+        searchString.map{ $0!.characters.count > 3 }
+                    .bindTo(validSearchText)
         
         /*
          ‘throttle’ the queries so that at most only one or two are sent per second.
@@ -31,6 +39,7 @@ class PhotoSearchViewModel {
                 [unowned self] text in
                 self.executeSearch(text!)
         }
+
     }
     
     func executeSearch(text: String) {
